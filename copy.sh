34848296e2949bc6fe8b411a89bfbfe54371796b
Gustavo <gustavohfcarvalho@gmail.com>
2017-03-12 02:34:42 -0300

    Scripts ignoram repositorios com .git no nome

--- copy.sh
+++ copy.sh
@@ -15,9 +15,11 @@ done
 # Copia os arquivos
 find $1/home/ -type f -print0 | while IFS= read -r -d '' arquivo
 do
-  arquivo=`echo $arquivo | cut -f 1 -d / --complement | cut -f 1 -d / --complement`
-  echo "$1/home/"$arquivo" -> ~/"$arquivo""
-  cp $1/home/"$arquivo" ~/"$arquivo"
+  if [ -n "${arquivo/%*.git*/}" ]; then
+    arquivo=`echo $arquivo | cut -f 1 -d / --complement | cut -f 1 -d / --complement`
+    echo "$1/home/"$arquivo" -> ~/"$arquivo""
+    cp $1/home/"$arquivo" ~/"$arquivo"
+  fi
 done
 
 exit 0
--- verify.sh
+++ verify.sh
@@ -2,15 +2,17 @@
 
 find $1/home/ -type f -print0 | while IFS= read -r -d '' arquivo
 do
-  arquivo=`echo $arquivo | cut -f 1 -d / --complement | cut -f 1 -d / --complement`
-  if [ -f ~/"$arquivo" ]; then
-    if [ "`md5sum ~/"$arquivo" | cut -f 1 -d ' '`" == "`md5sum $1/home/"$arquivo" | cut -f 1 -d ' '`" ]; then
-      echo "$arquivo - OK"
+  if [ -n "${arquivo/%*.git*/}" ]; then
+    arquivo=`echo $arquivo | cut -f 1 -d / --complement | cut -f 1 -d / --complement`
+    if [ -f ~/"$arquivo" ]; then
+      if [ "`md5sum ~/"$arquivo" | cut -f 1 -d ' '`" == "`md5sum $1/home/"$arquivo" | cut -f 1 -d ' '`" ]; then
+        echo "$arquivo - OK"
+      else
+        echo "$arquivo - Desatualizado *************************************************************"
+      fi
     else
-      echo "$arquivo - Desatualizado *************************************************************"
+      echo "$arquivo - Arquivo nao encontrado ******************************************************"
     fi
-  else
-    echo "$arquivo - Arquivo nao encontrado ******************************************************"
   fi
 done
 
