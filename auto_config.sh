ddaa1c8873f1d5aeef2dca1a10e8a797a2ec0e31
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-03-20 19:54:40 -0300

    Configuracao basica do powerline

--- auto_config.sh
+++ auto_config.sh
@@ -2,7 +2,7 @@
 
 # Nome de todas aplicacoes q podem ser configuradas, deixar espaco no inicio e
 # no final da string.
-aplicacoes=" i3 vim i3blocks bashrc tmux zshrc "
+aplicacoes=" i3 vim i3blocks bashrc tmux zshrc powerline_config "
 
 # Confere os parametros
 if [ "$#" -ne 3 ]; then
